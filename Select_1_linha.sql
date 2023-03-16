9c060657094d128b4cddf149bb25eb1720b15313
Ricardo Luciano Feijó Correa <37032801+ricardolucianofc@users.noreply.github.com>
2023-03-16 18:17:02 -0300

    Update Select_1_linha.sql

--- Select_1_linha.sql
+++ Select_1_linha.sql
@@ -3,3 +3,7 @@ SELECT TOP 1 REPLACE(REPLACE(REPLACE((
 SELECT campo AS XXX FROM TABELA
 
 FOR XML RAW('YYY')),'<YYY XXX="','' ),'"/>',', ') + '..', ', ..', '') AS Resultado
+
+--
+
+SELECT STUFF((SELECT ', ' + cidade FROM cidades FOR XML PATH('')), 1, 2, '') AS todas_cidades;
